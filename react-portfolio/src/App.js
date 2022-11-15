d5a239521eaa6ff9900f652e6a17c0e64f1cdc7c
Andrew <echowhite93@yahoo.com>
2022-11-14 17:38:44 -0800

    added routes for website

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './index.css'
+import Home from './routes/Home';
+import Projects from './routes/Projects';
+import About from './routes/About';
+import Contact from './routes/Contact';
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="projects" element={<Projects />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+    </Routes>
+    </>
   );
 }
 
