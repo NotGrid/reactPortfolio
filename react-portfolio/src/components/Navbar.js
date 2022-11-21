3e506db8244d898ae8f1e4e0347dc851a7d224fe
Andrew <echowhite93@yahoo.com>
2022-11-20 21:15:19 -0800

    created responsive function for navbar menu while in phone browser

--- react-portfolio/src/components/Navbar.js
+++ react-portfolio/src/components/Navbar.js
@@ -1,13 +1,16 @@
 import './Navbar.css';
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Navbar = () => {
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
     return <div className='header'>
         <Link to='/'>
             <h1>My Portfolio</h1>
         </Link>
-        <ul className='nav-menu'>
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
             <li>
                 <Link to='/'>Home</Link>
             </li>
@@ -21,6 +24,11 @@ const Navbar = () => {
                 <Link to='/contact'>Contact</Link>
             </li>
         </ul>
+        <div className='dropdown' onClick={handleClick}>
+            {click 
+            ? (<FaTimes size={20} style={{color: '#90ee90'}} />) 
+            : (<FaBars size={20} style={{color: '#90ee90'}} />)}
+        </div>
     </div>
 };
 
